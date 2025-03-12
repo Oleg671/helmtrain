c092bc1d50b5bbf10e291d6241d2b8ca7c6312a8
T-Owl <96847581+Oleg671@users.noreply.github.com>
2025-03-12 20:30:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
 
 token='8161978023:AAGOm1EDAzf8tPhO16fwk3ZUNKOUs7TEP9Y'
 bot = telebot.TeleBot(token)
-GROUP_ID=-1002324488081
-blacklist=["паучья", "хавальник", "паучью", "пауки", "паук", "призрачная", "эмилия", "ната", "щетинина", "берсика","берсику", "берсик", "нико", "админка", "шейминг", "закон", "законом", "ук", "украли", "украсть", "клевета", "паучьей", "баните", "отпишитесь", "отписаться", "подписчики", "эмилии", "срач", "шлюха", "пизда", "пиздец", "нахуй", "хуй", "шалава", "вагина"]
+GROUP_ID=[-1002324488081,-1002325350087]
+with open('./codeWords.txt', encoding='utf-8') as f:
+    blacklist = f.read().split(' ')
+# blacklist=["паучья", "хавальник", "паучью", "пауки", "паук", "призрачная", "эмилия", "ната", "щетинина", "берсика","берсику", "берсик", "нико", "админка", "шейминг", "закон", "законом", "ук", "украли", "украсть", "клевета", "паучьей", "баните", "отпишитесь", "отписаться", "подписчики", "эмилии", "срач", "шлюха", "пизда", "пиздец", "нахуй", "хуй", "шалава", "вагина"]
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     msg=message.text.lower()
     for x in blacklist:
-        if(x in msg.split(' ') and message.chat.id==GROUP_ID):
+        if(x in msg.split(' ') and message.chat.id in GROUP_ID):
             bot.delete_message(message.chat.id, message.message_id)
             print(datetime.now(pytz.timezone('Europe/Moscow')), "Message:", message.message_id, "-",message.text, message.from_user)
-        elif message.chat.id!=GROUP_ID:
+        elif message.chat.id not in GROUP_ID:
             info = "ChatID: "+str(message.chat.id)+"\nMessage: "+str(message.message_id)+" - "+str(message.text)+"\nFrom: "+str(message.from_user)
             bot.forward_message(1835147179, message.chat.id, message.message_id)
             bot.send_message(1835147179,info)
